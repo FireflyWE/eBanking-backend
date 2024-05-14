11bc6e14d15d3442280146fb18de2bb2121af19d
FireflyWE <wissalelkhettab@gmail.com>
2024-05-14 09:30:19 +0100

    Initial commit

---
+++ src/main/java/ma/emsi/ebankingbackend/services/BankService.java
@@ -0,0 +1,38 @@
+package ma.emsi.ebankingbackend.services;
+
+import ma.emsi.ebankingbackend.entities.BankAccount;
+import ma.emsi.ebankingbackend.entities.CurrentAccount;
+import ma.emsi.ebankingbackend.entities.SavingAccount;
+import ma.emsi.ebankingbackend.repositories.BankAccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class BankService {
+    @Autowired
+    private BankAccountRepository bankAccountRepository;
+
+    public void consulter(){
+        BankAccount bankAccount=bankAccountRepository.findById("498b0be2-556b-44a2-98ef-fff6a2f87de9").orElse(null);
+        if(bankAccount!=null) {
+            System.out.println("***************************");
+            System.out.println(bankAccount.getId());
+            System.out.println(bankAccount.getBalance());
+            System.out.println(bankAccount.getStatus());
+            System.out.println(bankAccount.getCreatedAt());
+            System.out.println(bankAccount.getCustomer().getName());
+            System.out.println(bankAccount.getClass().getSimpleName());
+            if (bankAccount instanceof CurrentAccount) {
+                System.out.println(((CurrentAccount) bankAccount).getOverDraft());
+            } else if (bankAccount instanceof SavingAccount) {
+                System.out.println(((SavingAccount) bankAccount).getInterestRate());
+            }
+            bankAccount.getAccountOperations().forEach(op -> {
+                System.out.println(op.getType() + "\t" + op.getOperationDate() + "\t" + op.getAmount());
+            });
+
+    }
+        }
+}
